2a669c803320caf0338524f1d0bde834a4dbaa27
Marián Konček <marian.koncek@mailbox.org>
2023-03-01 16:05:49 +0100

    Parallelize build

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ mkdir -p target/bin
 
 CXXFLAGS+=' -std=c++2a -Isrc -Wall -Wextra -Wpedantic'
 
-${CXX} ${CXXFLAGS} -o target/bin/java_remove_symbols src/java_remove_symbols.cpp ${LDFLAGS}
-${CXX} ${CXXFLAGS} -o target/bin/java_remove_symbols_test src/java_remove_symbols_test.cpp ${LDFLAGS}
+for source in java_remove_symbols java_remove_symbols_test; do
+	${CXX} ${CXXFLAGS} -o "target/bin/${source}" "src/${source}.cpp" ${LDFLAGS} &
+done; wait
