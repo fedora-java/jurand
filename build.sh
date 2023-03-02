055c8d35621780a2822cc9706daa06159871b517
Marián Konček <marian.koncek@mailbox.org>
2023-03-02 16:21:38 +0100

    Non-parallelize build

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ mkdir -p target/bin
 CXXFLAGS+=' -std=c++2a -Isrc -Wall -Wextra -Wpedantic'
 
 for source in java_remove_symbols java_remove_symbols_test; do
-	${CXX:-g++} ${CXXFLAGS} -o "target/bin/${source}" "src/${source}.cpp" ${LDFLAGS:-} &
-done; wait
+	${CXX:-g++} ${CXXFLAGS} -o "target/bin/${source}" "src/${source}.cpp" ${LDFLAGS:-}
+done
