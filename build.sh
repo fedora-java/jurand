afc226191af831eb5e3fd5d488ee2fbd3d063753
Marián Konček <marian.koncek@mailbox.org>
2023-03-05 16:51:18 +0100

    Separate LDFLAGS and LDLIBS

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ mkdir -p target/bin
 CXXFLAGS+=' -std=c++2a -Isrc -Wall -Wextra -Wpedantic'
 
 for source in jurand jurand_test; do
-	${CXX:-g++} ${CXXFLAGS} -o "target/bin/${source}" "src/${source}.cpp" ${LDFLAGS:-}
+	${CXX:-g++} ${CXXFLAGS} ${LDFLAGS:-} -o "target/bin/${source}" "src/${source}.cpp" ${LDLIBS:-}
 done
