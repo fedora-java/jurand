cfdb9e858dcc643b4eaacd95c222d1eb412877cc
Marián Konček <marian.koncek@mailbox.org>
2023-05-10 10:33:47 +0200

    Improve manpages

--- build_manpages.sh
+++ build_manpages.sh
@@ -6,7 +6,7 @@ mkdir -p target/manpages
 
 for manpage in manpages/*; do
 	manpage="${manpage##*/}"
-	manpage="${manpage%.txt}"
-	asciidoc -b docbook -d manpage -o "target/manpages/${manpage}.xml" "manpages/${manpage}.txt"
+	manpage="${manpage%.adoc}"
+	asciidoc -b docbook -d manpage -o "target/manpages/${manpage}.xml" "manpages/${manpage}.adoc"
 	xmlto man --skip-validation -o "target/manpages" "target/manpages/${manpage}.xml"
 done
